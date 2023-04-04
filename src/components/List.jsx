faadf957612f128e8dcbe06feb0ea0aa59efd7ed
Alexandra Chernova <estelelendil@mail.ru>
2023-04-04 13:02:34 +0300

    initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ComponentWithWrapper from './myWrapper.jsx';
+
+export default function List(props) {
+    return props.list.map(item => {
+        return <ComponentWithWrapper key={Math.random(100)} {...item} />
+    });
+};
+
+
